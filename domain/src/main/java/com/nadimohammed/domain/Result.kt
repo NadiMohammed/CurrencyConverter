e4bd1736e08911b7f55139df99cbde37f9e2ab32
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-02-28 21:24:47 +0200

    Create Result.kt
    create Result sealed class to handle all api success or failed

---
+++ domain/src/main/java/com/nadimohammed/domain/Result.kt
@@ -0,0 +1,6 @@
+package com.nadimohammed.domain
+
+sealed class Result<out T : Any> {
+    data class Success<out T : Any>(val results: T? = null) : Result<T>()
+    data class Failed(val exception: Exception? = null) : Result<Nothing>()
+}
