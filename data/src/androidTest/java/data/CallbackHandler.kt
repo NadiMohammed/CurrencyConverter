e10d1ffeecd100db2dbeb64f540b7ed06787794d
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-02-28 21:30:05 +0200

    Create CallbackHandler.kt
    create CallbackHandler to handle all API success response data and failure exception

---
+++ data/src/androidTest/java/data/CallbackHandler.kt
@@ -0,0 +1,29 @@
+package data
+
+import com.google.gson.Gson
+import com.nadimohammed.domain.Result
+import retrofit2.Response
+
+
+abstract class CallbackHandler {
+    suspend fun <T : Any> safeApiCall(call: suspend () -> Response<T>): Result<T> {
+        return try {
+            val response = call.invoke()
+            if (response.isSuccessful) {
+                Result.Success(response.body()!!)
+            } else {
+                Result.Failed(
+                    RuntimeException(
+                        Gson().fromJson(
+                            response.errorBody()?.string(),
+                            Result.Failed::class.java
+                        ).exception
+                    )
+                )
+            }
+
+        } catch (e: Exception) {
+            Result.Failed(e)
+        }
+    }
+}
