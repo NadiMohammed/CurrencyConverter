be91cba4dd51dafe1c06dedf815a55491f5d412e
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-02-28 23:01:36 +0200

    provide AppModule with Retrofit
    provide AppModule with Retrofit and HttpLoggingInterceptor

--- app/src/main/java/com/nadimohammed/currencyconverter/di/AppModule.kt
+++ app/src/main/java/com/nadimohammed/currencyconverter/di/AppModule.kt
@@ -1,11 +1,53 @@
 package com.nadimohammed.currencyconverter.di
 
+import com.nadimohammed.data.BuildConfig
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(createClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun createClient(): OkHttpClient {
+        val okHttpClientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val interceptor = Interceptor { chain ->
+                chain.proceed(
+                    chain.request().newBuilder().url(
+                        chain.request().url.newBuilder()
+                            .addQueryParameter("access_key", BuildConfig.API_KEY)
+                            .build()
+                    ).build()
+                )
+            }
+            okHttpClientBuilder.addInterceptor(loggingInterceptor)
+            okHttpClientBuilder.addInterceptor(interceptor)
+        }
+        return okHttpClientBuilder.build()
+    }
+
 
 }
