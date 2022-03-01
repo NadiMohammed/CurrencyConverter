cb5bfbd97c5b5bb306008d0eeacd930f3635dd71
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-01 11:45:44 +0200

    Creat DataBinding BaseFragment
    I have created a BaseFragment so that it creates a ViewDataBinding for us and we inherit from it so we don't need to initialize the binding variables in every activity or fragment

---
+++ app/src/main/java/com/nadimohammed/currencyconverter/ui/currencyconverter/BaseFragment.kt
@@ -0,0 +1,29 @@
+package com.nadimohammed.currencyconverter.ui.currencyconverter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewDataBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = getDataBinding()
+        return binding.root
+    }
+
+    abstract fun getDataBinding(): VB
+
+    internal fun firstTimeCreated(savedInstanceState: Bundle?) = savedInstanceState == null
+
+}
