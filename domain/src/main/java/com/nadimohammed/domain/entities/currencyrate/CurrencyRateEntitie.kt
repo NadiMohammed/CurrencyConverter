e71aef45158a99da855ce8c48a505ac96435b9d7
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-01 23:46:50 +0200

    Fix Entitie
    Use CurrencyRateEntitie correctly

--- domain/src/main/java/com/nadimohammed/domain/entities/currencyrate/CurrencyRateEntitie.kt
+++ domain/src/main/java/com/nadimohammed/domain/entities/currencyrate/CurrencyRateEntitie.kt
@@ -3,8 +3,8 @@ package com.nadimohammed.domain.entities.currencyrate
 data class CurrencyRateEntitie(
     val base: String? = "",
     val date: String? = "",
-//    val rates: Rates? = Rates(),
-    val rates: List<Rates> = listOf(),
+    val rates: Rates? = Rates(),
+//    val rates: List<Rates> = listOf(),
     val success: Boolean? = false,
     val timestamp: Int? = 0
 )
