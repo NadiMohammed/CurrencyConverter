307a8ce8c3b8091770cc0eda11a077a0eaddde90
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-01 17:39:23 +0200

    Update AppModule.kt
    providesOkHttpClient with Retrofit

--- app/src/main/java/com/nadimohammed/currencyconverter/di/AppModule.kt
+++ app/src/main/java/com/nadimohammed/currencyconverter/di/AppModule.kt
@@ -1,6 +1,11 @@
 package com.nadimohammed.currencyconverter.di
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.nadimohammed.data.BuildConfig
+import com.nadimohammed.data.api.CurrencyApi
+import com.nadimohammed.data.repository.CurrencyRemoteDataSource
+import com.nadimohammed.domain.repository.CurrencyRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,35 +24,54 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .client(createClient())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     }
 
     @Provides
     @Singleton
-    fun createClient(): OkHttpClient {
-        val okHttpClientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
-        if (BuildConfig.DEBUG) {
-            val loggingInterceptor =
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            val interceptor = Interceptor { chain ->
-                chain.proceed(
-                    chain.request().newBuilder().url(
-                        chain.request().url.newBuilder()
-                            .addQueryParameter("access_key", BuildConfig.API_KEY)
-                            .build()
-                    ).build()
-                )
-            }
-            okHttpClientBuilder.addInterceptor(loggingInterceptor)
-            okHttpClientBuilder.addInterceptor(interceptor)
+    fun providesInterceptor(): Interceptor =
+        Interceptor { chain ->
+            chain.proceed(
+                chain.request().newBuilder().url(
+                    chain.request().url.newBuilder()
+                        .addQueryParameter("access_key", BuildConfig.API_KEY)
+                        .build()
+                ).build()
+            )
         }
-        return okHttpClientBuilder.build()
-    }
 
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient =
+        OkHttpClient()
+            .newBuilder()
+            .addInterceptor(providesInterceptor())
+            .addInterceptor(providesLoggingInterceptor())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(providesOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+
+
+    @Singleton
+    @Provides
+    fun provideCurrencyApi(retrofit: Retrofit): CurrencyApi = retrofit.create(CurrencyApi::class.java)
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule {
+
+    @Binds
+    abstract fun bindCurrencyRepository(currencyRemoteDataSource: CurrencyRemoteDataSource): CurrencyRepository
 
 }
