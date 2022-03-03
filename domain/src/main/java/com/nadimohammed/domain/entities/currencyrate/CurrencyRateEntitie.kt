df8126e1a61dca132c3c4247669ea24b7f4e9322
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-03 17:42:03 +0200

    remove unused entitie

--- domain/src/main/java/com/nadimohammed/domain/entities/currencyrate/CurrencyRateEntitie.kt
+++ domain/src/main/java/com/nadimohammed/domain/entities/currencyrate/CurrencyRateEntitie.kt
@@ -4,7 +4,6 @@ data class CurrencyRateEntitie(
     val base: String? = "",
     val date: String? = "",
     val rates: Rates? = Rates(),
-//    val rates: List<Rates> = listOf(),
     val success: Boolean? = false,
     val timestamp: Int? = 0
 )
