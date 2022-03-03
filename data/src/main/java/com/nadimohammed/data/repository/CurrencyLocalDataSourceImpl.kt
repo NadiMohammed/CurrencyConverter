dfe600449326061020037f68d83405603081f9ce
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-03 18:04:24 +0200

    Update CurrencyLocalDataSourceImpl.kt

--- data/src/main/java/com/nadimohammed/data/repository/CurrencyLocalDataSourceImpl.kt
+++ data/src/main/java/com/nadimohammed/data/repository/CurrencyLocalDataSourceImpl.kt
@@ -1,20 +1,20 @@
 package com.nadimohammed.data.repository
 
-import com.nadimohammed.data.CallbackHandler
+import com.nadimohammed.data.db.DatabaseCurrency
 import com.nadimohammed.data.db.MainDatabase
-import com.nadimohammed.domain.entities.currencyrate.CurrencyRateEntitie
-import com.nadimohammed.domain.repository.CurrencyLocalRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class CurrencyLocalDataSourceImpl @Inject constructor(private val database: MainDatabase) :
-    CurrencyLocalRepository, CallbackHandler() {
+class CurrencyLocalDataSourceImpl @Inject constructor(private val database: MainDatabase) {
 
-    override suspend fun create(currency: List<CurrencyRateEntitie>) {
-        database.currencyDAO.insert()
+    suspend fun create(currency: DatabaseCurrency) {
+        database.currencyDAO.insert(currency)
     }
 
-    override suspend fun get(): List<CurrencyRateEntitie> = database.currencyDAO.get()
+    suspend fun get(): List<DatabaseCurrency> {
+        return database.currencyDAO.read()
+    }
+
 
 }
