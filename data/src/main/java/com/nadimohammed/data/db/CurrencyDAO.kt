be4e47d1230fcbc09e7bb2750d7e942f3aa29e25
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-03 17:48:26 +0200

    Update insert() and read()
    Update insert() and read() to bring data correctly

--- data/src/main/java/com/nadimohammed/data/db/CurrencyDAO.kt
+++ data/src/main/java/com/nadimohammed/data/db/CurrencyDAO.kt
@@ -1,12 +1,15 @@
 package com.nadimohammed.data.db
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface CurrencyDAO {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(databaseCurrency: List<DatabaseCurrency>)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(databaseCurrency: DatabaseCurrency)
 
     @Query("SELECT * FROM currency")
-    suspend fun get(): List<DatabaseCurrency>
+    suspend fun read(): List<DatabaseCurrency>
 }
