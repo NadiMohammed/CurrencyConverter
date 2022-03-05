124aa78ec46dc8bb7b1aa0ae5d49d1b691ac7839
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-05 20:07:08 +0200

    add getOtherCurrencyRate()

--- data/src/main/java/com/nadimohammed/data/api/CurrencyApi.kt
+++ data/src/main/java/com/nadimohammed/data/api/CurrencyApi.kt
@@ -2,6 +2,7 @@ package com.nadimohammed.data.api
 
 
 import com.nadimohammed.domain.entities.currencyrate.CurrencyRateEntitie
+import com.nadimohammed.domain.entities.othercurrencyrate.OtherCurrencyRate
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -9,4 +10,6 @@ interface CurrencyApi {
     @GET("latest")
     suspend fun getCurrencyRate(): Response<CurrencyRateEntitie>
 
+    @GET("latest")
+    suspend fun getOtherCurrencyRate(): Response<OtherCurrencyRate>
 }
