17b6c0e2f406b0de4ede5566517a9e128893e941
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-05 19:58:17 +0200

    add getOtherCurrencyRate Repository

--- domain/src/main/java/com/nadimohammed/domain/repository/CurrencyRepository.kt
+++ domain/src/main/java/com/nadimohammed/domain/repository/CurrencyRepository.kt
@@ -2,7 +2,10 @@ package com.nadimohammed.domain.repository
 
 import com.nadimohammed.domain.Result
 import com.nadimohammed.domain.entities.currencyrate.CurrencyRateEntitie
+import com.nadimohammed.domain.entities.othercurrencyrate.OtherCurrencyRate
 
 interface CurrencyRepository {
     suspend fun getCurrencyRate() : Result<CurrencyRateEntitie>
+
+    suspend fun getOtherCurrencyRate() : Result<OtherCurrencyRate>
 }
