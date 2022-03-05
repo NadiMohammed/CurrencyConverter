a83ef2f2514276cdff66ba6dbed5c380f850b73e
Nadi Mohammed <Nadimohammed94@gmail.com>
2022-03-05 20:08:40 +0200

    create OtherCurrencyRate entities

---
+++ domain/src/main/java/com/nadimohammed/domain/entities/othercurrencyrate/OtherCurrencyRate.kt
@@ -0,0 +1,5 @@
+package com.nadimohammed.domain.entities.othercurrencyrate
+
+data class OtherCurrencyRate(
+    val rates: OtherRates? = OtherRates()
+)
---
+++ domain/src/main/java/com/nadimohammed/domain/entities/othercurrencyrate/OtherRates.kt
@@ -0,0 +1,14 @@
+package com.nadimohammed.domain.entities.othercurrencyrate
+
+data class OtherRates(
+    val USD: Double? = 0.0,
+    val EGP: Double? = 0.0,
+    val EUR: Int? = 0,
+    val GBP: Double? = 0.0,
+    val BTC: Double? = 0.0,
+    val SAR: Double? = 0.0,
+    val QAR: Double? = 0.0,
+    val AED: Double? = 0.0,
+    val CAD: Double? = 0.0,
+    val CHF: Double? = 0.0,
+)
